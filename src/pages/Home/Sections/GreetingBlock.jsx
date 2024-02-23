19ff880b237283d51ec15d2acea0fef950aecd79
Aisuluu0507 <aisuulu0507@gmail.com>
2024-02-24 00:01:17 +0600

    finished loyout with greetingBlock

--- src/pages/Home/Sections/GreetingBlock.jsx
+++ src/pages/Home/Sections/GreetingBlock.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "@styles/pages/Home/GreetingBlock.module.scss"
-import carImg from "./images/car.png"
+import carImg from "@images/car.png"
 
 const GreetingBlock = () => {
   return (
