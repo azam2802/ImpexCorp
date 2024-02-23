a4b3854f1155c7c514d50f54afb60b747dc5ea34
azam2802 <azamumarov182@gmal.com>
2024-02-23 19:52:35 +0600

    add some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from "react"
 import "./App.scss"
 import { Suspense } from "react"
-import { useTranslation } from "react-i18next"
 import Header from "@layout/Header/Header"
 import Footer from "@layout/Footer/Footer"
 import BackToTop from "@ui/BackToTop/BackToTop"
 import Home from "@pages/Home/Home"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-  const { t } = useTranslation()
-
   return (
     <Suspense fallback="Loading...">
       <div className="App">
         <Header />
-        <h1 style={{ marginBottom: "1000px" }}>{t("text")}</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
         <BackToTop />
-        <Home />
         <Footer />
       </div>
     </Suspense>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import ApplicationForm from "./Sections/ApplicationForm"
+import { useTranslation } from "react-i18next"
 
 export const Home = () => {
+  const { t } = useTranslation()
+
   return (
     <main>
+      <h1 style={{ marginBottom: "1000px" }}>{t("text")}</h1>
       <ApplicationForm />
     </main>
   )
