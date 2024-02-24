350ccba4071758b602994c064583e486f2aae338
Aisuluu0507 <aisuulu0507@gmail.com>
2024-02-24 13:35:46 +0600

    chenged the layout

--- src/pages/Home/Sections/GreetingBlock.jsx
+++ src/pages/Home/Sections/GreetingBlock.jsx
@@ -5,17 +5,23 @@ import carImg from "@images/car.png"
 const GreetingBlock = () => {
   return (
     <div>
-      <div className={s["mainBlock"]}>
-        <h2 className={s["title"]}>Добро пожаловать в автосалон Impex Corp</h2>
-        <p className={s["text"]}>
-          Impex Corp — это авто компания, которая предлагает своим клиентам
-          широкий ассортимент товаров и услуг, связанных с автомобильной
-          индустрией. Став ведущим игроком на рынке, Impex Corp обладает
-          несколькими ключевыми преимуществами, которые делают их идеальным
-          выбором для покупателей
-        </p>
-        <img className={s["carImg"]} src={carImg} alt="Car" />
-        <button className={s["button"]}>Подробнее</button>
+      <div className={s["parentBlock"]}>
+        <div className={s["firstBlock"]}>
+          <img className={s["carImg"]} src={carImg} alt="Car" />
+        </div>
+        <div className="secondBlock">
+          <h2 className={s["title"]}>
+            Добро пожаловать в автосалон Impex Corp{" "}
+          </h2>
+          <p className={s["text"]}>
+            Impex Corp — это авто компания, которая предлагает своим клиентам
+            широкий ассортимент товаров и услуг, связанных с автомобильной
+            индустрией. Став ведущим игроком на рынке, Impex Corp обладает
+            несколькими ключевыми преимуществами, которые делают их идеальным
+            выбором для покупателей
+          </p>
+          <button className={s["button"]}>Подробнее</button>
+        </div>
       </div>
     </div>
   )
