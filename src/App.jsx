b1ebb61854ef0c58f65290d2076f77ff7b655480
azam2802 <azamumarov182@gmal.com>
2024-03-05 14:00:57 +0600

    routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import Home from "@pages/Home/Home"
 import React, { Suspense } from "react"
 import { Route, Routes } from "react-router-dom"
 import "./App.scss"
+import AboutUs from "@pages/AboutUs/AboutUs"
 
 const App = () => {
+  const router = [
+    {
+      path: "aboutus",
+      element: <AboutUs />,
+    },
+  ]
+
   return (
     <Suspense fallback="Loading...">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          {router.map((item, id) => (
+            <Route key={id} path={item.path} element={item.element} />
+          ))}
           <Route path="*" element={<h1>ERROR 404</h1>} />
         </Route>
       </Routes>
