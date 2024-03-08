f4abb67e1bcf9f5a30325529c5575cec9928c9a4
azam2802 <azamumarov182@gmal.com>
2024-03-09 00:10:35 +0600

    work with header

--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,7 +1,22 @@
 import React from "react"
+import s from "@styles/components/BurgerMenu.module.scss"
+import Logo from "@ui/Logo/Logo"
 
 const BurgerMenu = () => {
-  return <div></div>
+  return (
+    <nav className={s["mob-nav"]}>
+      <div className={s.row}>
+        <div className={s["col-6"]}>
+          <Logo />
+        </div>
+        <div className={s["col-6"]}>
+          <div className={s["burger-btn"]}>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default BurgerMenu
--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,40 +1,58 @@
+import BurgerMenu from "@components/BurgerMenu/BurgerMenu"
 import Catalog from "@components/Catalog/Catalog"
 import OverNavbar from "@components/OverNavbar/OverNavbar"
-// import Logo from "@images/Logo.svg"
 import s from "@styles/layout/Header.module.scss"
-import BurgerMenuBtn from "@ui/BurgerMenuBtn/BurgerMenuBtn"
+import Logo from "@ui/Logo/Logo"
 import React, { useState } from "react"
 // import { useTranslation } from "react-i18next"
-// import { CiSearch } from "react-icons/ci"
-// import { MdOutlineCancel } from "react-icons/md"
+import { IoSearch } from "react-icons/io5"
 // import { Link } from "react-router-dom"
 
 const Header = () => {
   // const { t } = useTranslation()
 
   const [showCatalog, setShowCatalog] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
   // const [showSearchIco, setShowSearchIco] = useState("")
+
+  const handleShowSearch = () => {
+    let search_ico = document.querySelector("#search_ico")
+    search_ico.classList.toggle(s.showSearch)
+    setShowSearch((prev) => !prev)
+    console.log(showSearch)
+  }
 
   // const onOpenCatalog = () => {
   //   setShowCatalog((prev) => !prev)
   // }
 
-  // const onCloseCatalog = () => {
-  //   setShowCatalog(false)
-  // }
+  const onCloseCatalog = () => {
+    setShowCatalog(false)
+    console.log("click")
+  }
 
   return (
     <div>
-      <header id="header">
+      <header className={s["pc-nav"]} id="header">
+        <OverNavbar />
         <nav className={s["pc-nav"]}>
-          <OverNavbar />
           <div className={s.row}>
-            <div className={s["col-6"]}></div>
+            <div className={s["col-6"]}>
+              <Logo onClick={onCloseCatalog} />
+              <div className={s.search_div}>
+                <button
+                  onClick={() => {
+                    onCloseCatalog, handleShowSearch
+                  }}>
+                  <IoSearch id="search_ico" />
+                </button>
+              </div>
+            </div>
             <div className={s["col-6"]}></div>
           </div>
         </nav>
-        <BurgerMenuBtn />
       </header>
+      <BurgerMenu />
       {showCatalog && <Catalog setShowCatalog={setShowCatalog} />}
     </div>
   )
