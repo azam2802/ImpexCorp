1606a6e6195b74c1e35f9363be4abbd9be1f8ebb
begimlandiya <erkinovabegimai05@gmail.com>
2024-03-12 13:08:36 +0600

    doing adaptive

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import App from "./App.jsx"
 import "./index.scss"
 import "./i18n.js"
 import { BrowserRouter } from "react-router-dom"
-;(function (w, d, u) {
-  var s = d.createElement("script")
-  s.async = true
-  s.src = u + "?" + ((Date.now() / 60000) | 0)
-  var h = d.getElementsByTagName("script")[0]
-  h.parentNode.insertBefore(s, h)
-})(
-  window,
-  document,
-  "https://cdn-ru.bitrix24.ru/b28492848/crm/site_button/loader_1_v57jnc.js",
-)
+// ;(function (w, d, u) {
+//   var s = d.createElement("script")
+//   s.async = true
+//   s.src = u + "?" + ((Date.now() / 60000) | 0)
+//   var h = d.getElementsByTagName("script")[0]
+//   h.parentNode.insertBefore(s, h)
+// })(
+//   window,
+//   document,
+//   "https://cdn-ru.bitrix24.ru/b28492848/crm/site_button/loader_1_v57jnc.js",
+// )
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
