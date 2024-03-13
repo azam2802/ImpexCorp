fff4a065f76cca3c27a27e5efb5da32a4f7389db
azam2802 <azamumarov182@gmal.com>
2024-03-14 02:29:20 +0600

    fix css

--- src/components/Carousels/Carousels.jsx
+++ src/components/Carousels/Carousels.jsx
@@ -1,18 +1,15 @@
-import React from "react"
-import "swiper/css"
-
-import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css/navigation"
-import "swiper/css/pagination"
-import "swiper/css/effect-fade"
-import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules"
-
-import { AiFillLeftSquare } from "react-icons/ai"
-import { AiFillRightSquare } from "react-icons/ai"
 import slide1 from "@images/frame1.png"
 import slide2 from "@images/frame2.png"
 import slide3 from "@images/frame3.png"
 import "@styles/components/Carousels.scss"
+import React from "react"
+import { AiFillLeftSquare, AiFillRightSquare } from "react-icons/ai"
+import "swiper/css"
+import "swiper/css/effect-fade"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
 
 const Carousels = () => {
   return (
