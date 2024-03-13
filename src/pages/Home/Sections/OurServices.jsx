356b0ea5d73a5911d13854d1013caa1100fb227c
Aisuluu0507 <aisuulu0507@gmail.com>
2024-03-13 22:52:05 +0600

    made changed

--- src/pages/Home/Sections/OurServices.jsx
+++ src/pages/Home/Sections/OurServices.jsx
@@ -5,76 +5,64 @@ import searchIcon from "@images/search.svg"
 import registrationIcon from "@images/registration.svg"
 import autoIcon from "@images/auto.svg"
 import insuranceIcon from "@images/insurance.svg"
-import styles from "@styles/pages/Home/OurServices.module.scss"
+import s from "@styles/pages/Home/OurServices.module.scss"
 
 const OurServices = () => {
   const { t } = useTranslation()
 
   return (
-    <div className={styles.parentContainer}>
+    <div className={s.parentContainer}>
       <h1>{t("HomePage.OurServices.title")}</h1>
-      <div className={styles.servicesContainer}>
+      <div className={s.servicesContainer}>
         <motion.div
-          initial={{ opacity: 0, x: -50 }}
+          initial={{ opacity: 0, x: -150 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className={styles.serviceItem}>
-          <img className={styles.searchImg} src={searchIcon} alt="search" />
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className={s.serviceItem}>
+          <img className={s.searchImg} src={searchIcon} alt="search" />
           <div>
-            <p className={styles.paragraph1}>
-              {t("HomePage.OurServices.search")}
-            </p>
-            <span className={styles.span1}>
-              {t("HomePage.OurServices.cars")}
-            </span>
+            <p className={s.paragraph1}>{t("HomePage.OurServices.search")}</p>
+            <p className={s.span1}>{t("HomePage.OurServices.cars")}</p>
           </div>
         </motion.div>
 
         <motion.div
-          initial={{ opacity: 0, x: 50 }}
+          initial={{ opacity: 0, x: 150 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className={styles.serviceItem}>
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className={s.serviceItem}>
           <img src={autoIcon} alt="auto" />
           <div>
-            <p className={styles.paragraph3}>
-              {t("HomePage.OurServices.delivery")}
-            </p>
-            <span className={styles.span3}>
-              {t("HomePage.OurServices.guarantee")}
-            </span>
+            <p className={s.paragraph3}>{t("HomePage.OurServices.delivery")}</p>
+            <p className={s.span3}>{t("HomePage.OurServices.guarantee")}</p>
           </div>
         </motion.div>
 
         <motion.div
-          initial={{ opacity: 0, x: -50 }}
+          initial={{ opacity: 0, x: -150 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className={styles.serviceItem}>
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className={s.serviceItem}>
           <img src={registrationIcon} alt="registration" />
           <div>
-            <p className={styles.paragraph2}>
+            <p className={s.paragraph2}>
               {t("HomePage.OurServices.registration")}
             </p>
-            <span className={styles.span2}>
-              {t("HomePage.OurServices.accounting")}
-            </span>
+            <p className={s.span2}>{t("HomePage.OurServices.accounting")}</p>
           </div>
         </motion.div>
 
         <motion.div
-          initial={{ opacity: 0, x: 50 }}
+          initial={{ opacity: 0, x: 150 }}
           animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className={styles.serviceItem}>
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className={s.serviceItem}>
           <img src={insuranceIcon} alt="insurance" />
           <div>
-            <p className={styles.paragraph4}>
+            <p className={s.paragraph4}>
               {t("HomePage.OurServices.insurance")}
             </p>
-            <span className={styles.span4}>
-              {t("HomePage.OurServices.auto")}
-            </span>
+            <p className={s.span4}>{t("HomePage.OurServices.auto")}</p>
           </div>
         </motion.div>
       </div>
