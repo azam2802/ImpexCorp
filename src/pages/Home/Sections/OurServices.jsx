a2d3a4579e613da97d4e724fe84a07ec596901c5
Aisuluu0507 <aisuulu0507@gmail.com>
2024-03-14 23:51:58 +0600

    animation

--- src/pages/Home/Sections/OurServices.jsx
+++ src/pages/Home/Sections/OurServices.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { motion } from "framer-motion"
 import { useTranslation } from "react-i18next"
 import searchIcon from "@images/search.svg"
 import registrationIcon from "@images/registration.svg"
@@ -9,116 +10,86 @@ import s from "@styles/pages/Home/OurServices.module.scss"
 const OurServices = () => {
   const { t } = useTranslation()
 
+  const AnimLeft = {
+    hidden: {
+      x: -150,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: custom * 0.3 },
+    }),
+  }
+
+  const AnimRight = {
+    hidden: {
+      x: 150,
+      opacity: 0,
+    },
+    visible: (custom) => ({
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: custom * 0.3 },
+    }),
+  }
+
   return (
-    <div className={s.parentContainer}>
-      <h1 className={s.serviceTitle}>{t("HomePage.OurServices.title")}</h1>
-      <div className={s.row}>
-        <div className={s.col_6}>
-          <div className={s.imgDiv}>
-            <img src={searchIcon} alt="search" />
-          </div>
-          <div>
-            <p className={s.paragraph1}>{t("HomePage.OurServices.search")}</p>
-            <p className={s.span1}>{t("HomePage.OurServices.cars")}</p>
-          </div>
+    <section>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        className={s.parentContainer}>
+        <motion.h1 custom={1} variants={AnimLeft} className={s.serviceTitle}>
+          {t("HomePage.OurServices.title")}
+        </motion.h1>
+        <div className={s.row}>
+          <motion.div custom={2} variants={AnimLeft} className={s.col_6}>
+            <div className={s.imgDiv}>
+              <img src={searchIcon} alt="search" />
+            </div>
+            <div>
+              <p className={s.paragraph1}>{t("HomePage.OurServices.search")}</p>
+              <p className={s.span1}>{t("HomePage.OurServices.cars")}</p>
+            </div>
+          </motion.div>
+          <motion.div custom={2} variants={AnimLeft} className={s.col_6}>
+            <div className={s.imgDiv}>
+              <img src={autoIcon} alt="auto" />
+            </div>
+            <div>
+              <p className={s.paragraph3}>
+                {t("HomePage.OurServices.delivery")}
+              </p>
+              <p className={s.span3}>{t("HomePage.OurServices.guarantee")}</p>
+            </div>
+          </motion.div>
+          <motion.div custom={3} variants={AnimRight} className={s.col_6}>
+            <div className={s.imgDiv}>
+              <img src={registrationIcon} alt="registration" />
+            </div>
+            <div>
+              <p className={s.paragraph2}>
+                {t("HomePage.OurServices.registration")}
+              </p>
+              <p className={s.span2}>{t("HomePage.OurServices.accounting")}</p>
+            </div>
+          </motion.div>
+          <motion.div custom={3} variants={AnimRight} className={s.col_6}>
+            <div className={s.imgDiv}>
+              <img src={insuranceIcon} alt="insurance" />
+            </div>
+            <div>
+              <p className={s.paragraph4}>
+                {t("HomePage.OurServices.insurance")}
+              </p>
+              <p className={s.span4}>{t("HomePage.OurServices.auto")}</p>
+            </div>
+          </motion.div>
         </div>
-        <div className={s.col_6}>
-          <div className={s.imgDiv}>
-            <img src={autoIcon} alt="auto" />
-          </div>
-          <div>
-            <p className={s.paragraph3}>{t("HomePage.OurServices.delivery")}</p>
-            <p className={s.span3}>{t("HomePage.OurServices.guarantee")}</p>
-          </div>
-        </div>
-        <div className={s.col_6}>
-          <div className={s.imgDiv}>
-            <img src={registrationIcon} alt="registration" />
-          </div>
-          <div>
-            <p className={s.paragraph2}>
-              {t("HomePage.OurServices.registration")}
-            </p>
-            <p className={s.span2}>{t("HomePage.OurServices.accounting")}</p>
-          </div>
-        </div>
-        <div className={s.col_6}>
-          <div className={s.imgDiv}>
-            <img src={insuranceIcon} alt="insurance" />
-          </div>
-          <div>
-            <p className={s.paragraph4}>
-              {t("HomePage.OurServices.insurance")}
-            </p>
-            <p className={s.span4}>{t("HomePage.OurServices.auto")}</p>
-          </div>
-        </div>
-      </div>
-
-      {/* <div className={s.servicesContainer}>
-     
-
-        <motion.div
-          initial={{ opacity: 0, x: 150 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          className={s.serviceItem}>
-          <div className={s.imgDiv}>
-            <img src={autoIcon} alt="auto" />
-          </div>
-          <div>
-            <p className={s.paragraph3}>{t("HomePage.OurServices.delivery")}</p>
-            <p className={s.span3}>{t("HomePage.OurServices.guarantee")}</p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, x: -150 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          className={s.serviceItem}>
-          <div className={s.imgDiv}>
-            <img src={registrationIcon} alt="registration" />
-          </div>
-          <div>
-            <p className={s.paragraph2}>
-              {t("HomePage.OurServices.registration")}
-            </p>
-            <p className={s.span2}>{t("HomePage.OurServices.accounting")}</p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, x: 150 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          className={s.serviceItem}>
-          <div className={s.imgDiv}>
-            <img src={insuranceIcon} alt="insurance" />
-          </div>
-          <div>
-            <p className={s.paragraph4}>
-              {t("HomePage.OurServices.insurance")}
-            </p>
-            <p className={s.span4}>{t("HomePage.OurServices.auto")}</p>
-          </div>
-        </motion.div>
-
-        <motion.div
-          initial={{ opacity: 0, x: -150 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-          className={s.serviceItem}>
-          <div className={s.imgDiv}>
-            <img src={searchIcon} alt="search" />
-          </div>
-          <div>
-            <p className={s.paragraph1}>{t("HomePage.OurServices.search")}</p>
-            <p className={s.span1}>{t("HomePage.OurServices.cars")}</p>
-          </div>
-        </motion.div> 
-      </div> */}
-    </div>
+      </motion.div>
+    </section>
   )
 }
 
