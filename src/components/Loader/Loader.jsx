2f5966a205802c33a4ad8c81a4bedeb602d627f4
azam2802 <azamumarov182@gmal.com>
2024-03-21 21:17:24 +0600

    Fix Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import "@styles/components/Loader.scss"
 
 const Loader = () => {
-  return <div className="lds-dual-ring"></div>
+  return (
+    <div className="lds-dual-ring">
+      <h2>Loding...</h2>
+    </div>
+  )
 }
 
 export default Loader
