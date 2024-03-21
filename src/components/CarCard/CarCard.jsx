0d901f5700ccb97a2a57cac67f8d4420c2f5e98f
erkinov26 <erkinov0247@gmail.com>
2024-03-21 20:42:10 +0500

    some changed

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -12,37 +12,37 @@ export const CarCard = () => {
   const { t } = useTranslation()
   return (
     <li className={s["carCardItem"]}>
-      <img src={carImage} className={s["car-image"]} alt="" />
-      <h1 className={s["car-name"]}>BYD Tang II</h1>
-      <ul className={s["car-struct-list"]}>
-        <li className={s["car-struct-list-item"]}>
+      <img src={carImage} className={s["car_image"]} alt="" />
+      <h1 className={s["car_name"]}>BYD Tang II</h1>
+      <ul className={s["car_struct_list"]}>
+        <li className={s["car_struct_list_item"]}>
           <FaRegCalendar />
-          <p className={s["car-struct-text"]}>2023</p>
+          <p className={s["car_struct_text"]}>2023</p>
         </li>
-        <li className={s["car-struct-list-item"]}>
-          <img className={s["car-struct-image"]} src={Icon2} alt="" />
-          <p className={s["car-struct-text"]}>Авто</p>
+        <li className={s["car_struct_list_item"]}>
+          <img className={s["car_struct_image"]} src={Icon2} alt="" />
+          <p className={s["car_struct_text"]}>Авто</p>
         </li>
-        <li className={s["car-struct-list-item"]}>
+        <li className={s["car_struct_list_item"]}>
           <LuFuel />
-          <p className={s["car-struct-text"]}>Электро</p>
+          <p className={s["car_struct_text"]}>Электро</p>
         </li>
-        <li className={s["car-struct-list-item"]}>
+        <li className={s["car_struct_list_item"]}>
           <MdOutlineSpeed />
-          <p className={s["car-struct-text"]}>12.500</p>
+          <p className={s["car_struct_text"]}>12.500</p>
         </li>
-        <li className={s["car-struct-list-item"]}>
+        <li className={s["car_struct_list_item"]}>
           <FaFlag />
-          <p className={s["car-struct-text"]}>Китай</p>
+          <p className={s["car_struct_text"]}>Китай</p>
         </li>
-        <li className={s["car-struct-list-item"]}>
-          <img className={s["car-struct-image"]} src={Icon1} alt="" />
-          <p className={s["car-struct-text"]}>2.5</p>
+        <li className={s["car_struct_list_item"]}>
+          <img className={s["car_struct_image"]} src={Icon1} alt="" />
+          <p className={s["car_struct_text"]}>2.5</p>
         </li>
       </ul>
       <hr />
-      <div className={s["car-info"]}>
-        <h1 className={s["car-price"]}>$ 120000</h1>
+      <div className={s["car_info"]}>
+        <h1 className={s["car_price"]}>$ 120000</h1>
         <button className={s["moreButton"]}>
           {t("HomePage.CatalogBlock.buttonText")}
         </button>
