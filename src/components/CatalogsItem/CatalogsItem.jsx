87cc9c8b8e5a297ef26b8d97c7ef361a171ee8ea
erkinov26 <erkinov0247@gmail.com>
2024-04-10 13:39:44 +0500

    Catalogs section scroll through x axis

--- src/components/CatalogsItem/CatalogsItem.jsx
+++ src/components/CatalogsItem/CatalogsItem.jsx
@@ -5,7 +5,10 @@ import PropTypes from "prop-types"
 import { motion } from "framer-motion"
 import { BsChevronRight } from "react-icons/bs"
 import { Link } from "react-router-dom"
-
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FreeMode } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/free-mode"
 export const CatalogsItem = ({ catalogTitle, data }) => {
   const AnimBottom = {
     hidden: {
@@ -29,23 +32,32 @@ export const CatalogsItem = ({ catalogTitle, data }) => {
         {catalogTitle}
       </motion.h1>
       <motion.ul variants={AnimBottom} custom={1} className={s.car_card_list}>
-        {data.slice(0, 6).map((car) => (
-          <CarCard
-            key={car.car_slug}
-            images={`http://209.38.228.54:81/${car.images[0].image}`}
-            car_name={car.car_name}
-            price={car.price}
-            volume={car.volume}
-            transmission={car.transmission}
-            country={car.country_of_assembly}
-            mileage={car.mileage}
-          />
-        ))}
-        <Link className={s.next_button} to="catalog">
-          <BsChevronRight
-            style={{ width: "80px", height: "100%", fill: "#19746b" }}
-          />
-        </Link>
+        <Swiper
+          slidesPerView={data.length > 1 ? 3 : 2.8}
+          style={{ width: "100%" }}
+          freeMode={true}
+          modules={[FreeMode]}>
+          {data.slice(0, 6).map((car) => (
+            <SwiperSlide key={car.car_slug}>
+              <CarCard
+                images={`http://209.38.228.54:81/${car.images[0].image}`}
+                car_name={car.car_name}
+                price={car.price}
+                volume={car.volume}
+                transmission={car.transmission}
+                country={car.country_of_assembly}
+                mileage={car.mileage}
+              />
+            </SwiperSlide>
+          ))}
+          <SwiperSlide>
+            <Link className={s.next_button} to="catalog">
+              <BsChevronRight
+                style={{ width: "80px", height: "100%", fill: "#19746b" }}
+              />
+            </Link>
+          </SwiperSlide>
+        </Swiper>
       </motion.ul>
     </motion.li>
   )
