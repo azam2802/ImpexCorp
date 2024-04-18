fa15cc20f2b53241c804fece62d3e1d5807bde67
begimlandiya <erkinovabegimai05@gmail.com>
2024-04-18 20:25:09 +0600

    C:/Program Files/Git/

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -7,17 +7,19 @@ import TelegramIcon from "@images/TelegramIcon.webp"
 import InstagramIcon from "@images/InstagramIcon.webp"
 import GmailIcon from "@images/GmailIcon.webp"
 import WhatsUpIcon from "@images/WhatsUpIcon.webp"
-
 const Footer = () => {
   const { t, i18n } = useTranslation()
   const currentLanguage = i18n.getResourceBundle(i18n.languages[0])
 
   const renderLinks = (menu) =>
-    Object.keys(menu).map((key) => (
-      <li key={key}>
-        <Link to="/"> {menu[key]}</Link>
-      </li>
-    ))
+    Object.keys(menu).map((key) => {
+      console.log(menu[key].path) // Добавляем консольный вывод
+      return (
+        <li key={key}>
+          <Link to={menu[key].path}>{menu[key]}</Link>
+        </li>
+      )
+    })
 
   const footerColumns = [
     {
@@ -31,12 +33,14 @@ const Footer = () => {
     {
       title: t("footer.branches.name"),
       menu: currentLanguage.footer.branches.menu,
+      path: "https://2gis.kg/bishkek/geo/15763234351111077?m=74.61276%2C42.870892%2F19.15",
     },
     {
       title: t("footer.contacts.name"),
       menu: currentLanguage.footer.contacts.menu,
     },
   ]
+  console.log(footerColumns)
 
   return (
     <footer>
@@ -48,7 +52,18 @@ const Footer = () => {
         {footerColumns.map((column, index) => (
           <div className={s[`row_${index + 1}`]} key={index}>
             <ul>
-              <li>{column.title}</li>
+              <li>
+                {column.path ? (
+                  <a
+                    href={column.path}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    {column.title}
+                  </a>
+                ) : (
+                  column.title
+                )}
+              </li>
               {renderLinks(column.menu)}
             </ul>
           </div>
@@ -58,26 +73,36 @@ const Footer = () => {
       <div className={s.social_media}>
         <ul>
           <li>
-            <Link to="https://t.me/impexcorpkg" target="_blank">
+            <a
+              href="https://t.me/impexcorpkg"
+              target="_blank"
+              rel="noopener noreferrer">
               <img src={TelegramIcon} alt="Telegram Icon" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link to="https://www.instagram.com/impex.corp.kg" target="_blank">
+            <a
+              href="https://www.instagram.com/impex.corp.kg"
+              target="_blank"
+              rel="noopener noreferrer">
               <img src={InstagramIcon} alt="Instagram Icon" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link to="mailto:impexcorpkg@gmail.com" target="_blank">
+            <a
+              href="mailto:impexcorpkg@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer">
               <img src={GmailIcon} alt="Gmail Icon" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link
-              to="https://api.whatsapp.com/send?phone=996500677633"
-              target="_blank">
+            <a
+              href="https://api.whatsapp.com/send?phone=996500677633"
+              target="_blank"
+              rel="noopener noreferrer">
               <img src={WhatsUpIcon} alt="WhatsApp Icon" />
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
@@ -85,9 +110,12 @@ const Footer = () => {
       <hr />
       <p id={s.madeby}>
         Made by{" "}
-        <Link to="https://geeks.kg/geeks-pro" target="_blank">
+        <a
+          href="https://geeks.kg/geeks-pro"
+          target="_blank"
+          rel="noopener noreferrer">
           GeeksPRO
-        </Link>
+        </a>
       </p>
     </footer>
   )
