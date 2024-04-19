d140060932f48c89956d3fd524890006528ba603
begimlandiya <erkinovabegimai05@gmail.com>
2024-04-19 15:01:56 +0600

    added logic for links to work

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -7,28 +7,34 @@ import TelegramIcon from "@images/TelegramIcon.webp"
 import InstagramIcon from "@images/InstagramIcon.webp"
 import GmailIcon from "@images/GmailIcon.webp"
 import WhatsUpIcon from "@images/WhatsUpIcon.webp"
+
 const Footer = () => {
   const { t, i18n } = useTranslation()
   const currentLanguage = i18n.getResourceBundle(i18n.languages[0])
 
-  const renderLinks = (menu) =>
-    Object.keys(menu).map((key) => {
-      console.log(menu[key].path) // Добавляем консольный вывод
-      return (
-        <li key={key}>
-          <Link to={menu[key].path}>{menu[key]}</Link>
-        </li>
-      )
-    })
+  const renderLinks = (menu, path) =>
+    Object.keys(menu).map((key) => (
+      <li key={key}>
+        {path.startsWith("http") ? (
+          <a href={path} target="_blank" rel="noopener noreferrer">
+            {menu[key]}
+          </a>
+        ) : (
+          <Link to={path}>{menu[key]}</Link>
+        )}
+      </li>
+    ))
 
   const footerColumns = [
     {
       title: t("footer.aboutus.name"),
       menu: currentLanguage.footer.aboutus.menu,
+      path: "/about",
     },
     {
       title: t("footer.support.name"),
       menu: currentLanguage.footer.support.menu,
+      path: "https://api.whatsapp.com/send?phone=996500677633",
     },
     {
       title: t("footer.branches.name"),
@@ -38,9 +44,9 @@ const Footer = () => {
     {
       title: t("footer.contacts.name"),
       menu: currentLanguage.footer.contacts.menu,
+      path: "tel:+996500677633",
     },
   ]
-  console.log(footerColumns)
 
   return (
     <footer>
@@ -52,19 +58,8 @@ const Footer = () => {
         {footerColumns.map((column, index) => (
           <div className={s[`row_${index + 1}`]} key={index}>
             <ul>
-              <li>
-                {column.path ? (
-                  <a
-                    href={column.path}
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    {column.title}
-                  </a>
-                ) : (
-                  column.title
-                )}
-              </li>
-              {renderLinks(column.menu)}
+              <li>{column.title}</li>
+              {renderLinks(column.menu, column.path)}
             </ul>
           </div>
         ))}
@@ -73,36 +68,26 @@ const Footer = () => {
       <div className={s.social_media}>
         <ul>
           <li>
-            <a
-              href="https://t.me/impexcorpkg"
-              target="_blank"
-              rel="noopener noreferrer">
+            <Link to="https://t.me/impexcorpkg" target="_blank">
               <img src={TelegramIcon} alt="Telegram Icon" />
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="https://www.instagram.com/impex.corp.kg"
-              target="_blank"
-              rel="noopener noreferrer">
+            <Link to="https://www.instagram.com/impex.corp.kg" target="_blank">
               <img src={InstagramIcon} alt="Instagram Icon" />
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="mailto:impexcorpkg@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer">
+            <Link to="mailto:impexcorpkg@gmail.com" target="_blank">
               <img src={GmailIcon} alt="Gmail Icon" />
-            </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="https://api.whatsapp.com/send?phone=996500677633"
-              target="_blank"
-              rel="noopener noreferrer">
+            <Link
+              to="https://api.whatsapp.com/send?phone=996500677633"
+              target="_blank">
               <img src={WhatsUpIcon} alt="WhatsApp Icon" />
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
@@ -110,12 +95,9 @@ const Footer = () => {
       <hr />
       <p id={s.madeby}>
         Made by{" "}
-        <a
-          href="https://geeks.kg/geeks-pro"
-          target="_blank"
-          rel="noopener noreferrer">
+        <Link to="https://geeks.kg/geeks-pro" target="_blank">
           GeeksPRO
-        </a>
+        </Link>
       </p>
     </footer>
   )
