2bc27e2ad67ab35b02b0f58f71e9c1829d4701e6
azam2802 <azamumarov182@gmal.com>
2024-04-26 04:03:39 +0600

    fix: replaced querrySelector with useRef

--- src/ui/BackToTop/BackToTop.jsx
+++ src/ui/BackToTop/BackToTop.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { PiCaretDoubleUpThin } from "react-icons/pi"
 import s from "@styles/ui/BackToTop.module.scss"
 import { useLocation } from "react-router-dom"
 
 const BackToTop = () => {
-  const { pathname } = useLocation()
+  const { location } = useLocation()
+  const ref = useRef(null)
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      let scroll = document.querySelector("#top-btn")
-      scroll.classList.toggle(s.active, window.scrollY > 800)
+      ref.current.classList.toggle(s.active, window.scrollY > 800)
     })
   })
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [pathname])
+  }, [location.key])
 
   const toTop = () => {
     let currentPosition = window.scrollY
@@ -26,7 +26,7 @@ const BackToTop = () => {
   }
 
   return (
-    <div className={s.top_btn} id="top-btn" onClick={toTop}>
+    <div className={s.top_btn} id="top-btn" onClick={toTop} ref={ref}>
       <PiCaretDoubleUpThin alt="top-btn" />
     </div>
   )
