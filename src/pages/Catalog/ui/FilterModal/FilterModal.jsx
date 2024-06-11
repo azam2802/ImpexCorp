4e7d4b5e7c15c6ec2c4639245d94514e04547f61
begimlandiya <erkinovabegimai05@gmail.com>
2024-06-11 15:57:52 +0600

    C:/Program Files/Git/

--- src/pages/Catalog/ui/FilterModal/FilterModal.jsx
+++ src/pages/Catalog/ui/FilterModal/FilterModal.jsx
@@ -69,6 +69,7 @@ export const FiltrModal = ({ setOpenModal }) => {
   const handleInputChange = (event) => {
     const { name, value } = event.target
     getData(value, name)
+    console.log("Изменение фильтра:", name, value)
   }
 
   return (
